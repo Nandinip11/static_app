7496d1a226a1b3faa2a0e79f9f2a5a0af35d6901
Nandini <nandini.parashar@payu.in>
2017-02-06 13:44:31 +0530

    cghv

---
+++ test/models/user_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com", 
+     password: "foobar", password_confirmation: "foobar")
+  end
+  
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+        assert archer.followers.include?(michael)
+
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end
+
+end
